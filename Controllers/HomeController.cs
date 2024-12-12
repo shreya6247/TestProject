fbff0fa80f02a95b450e6afd5dc3ce08e593fc8c
unknown <Administrator@IQ-TEST2.mccit.local>
2024-12-12 12:58:45 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Q1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Q1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Details()
+        {
+            ViewData["msg"] = TempData["msg"];
+            ViewData["msg2"] = TempData["msg2"];
+            PassengerBusinessLogic ps = new PassengerBusinessLogic();
+            List<Passenger> list = new List<Passenger>();
+            list = ps.GetDetails();
+            return View(list);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Passenger pass)
+        {
+            PassengerBusinessLogic ps = new PassengerBusinessLogic();
+            TempData["msg"]=ps.InsertPassengers(pass);
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            PassengerBusinessLogic ps = new PassengerBusinessLogic();
+            TempData["msg2"] = ps.DeletePassenger(id);
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            PassengerBusinessLogic ps = new PassengerBusinessLogic();
+            Passenger pass=new Passenger();
+            pass = ps.StoreDetail(id);
+            return View(pass);
+        }
+        [HttpPost]
+        public ActionResult Edit(Passenger pass,int id)
+        {
+            PassengerBusinessLogic ps = new PassengerBusinessLogic();
+            TempData["msg"]=ps.UpdatePassenger(id, pass);
+            return RedirectToAction("Details");
+        }
+    }
+}
---
+++ Models/PassengerBusinessLogic.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Q1.Models
+{
+    public class PassengerBusinessLogic
+    {
+        static string str = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
+        SqlConnection con = new SqlConnection(str);
+        public List<Passenger> GetDetails()
+        {
+            List<Passenger> list = new List<Passenger>();
+            SqlCommand cmd = new SqlCommand("sp_fetchDetails", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while (rd.Read())
+            {
+                Passenger ps = new Passenger();
+
+                ps.Id = Convert.ToInt32(rd["p_id"]);
+                ps.Name = rd["p_name"].ToString();
+                ps.Address = rd["address"].ToString();
+                ps.Phone = rd["ph_no"].ToString();
+                ps.Email = rd["email"].ToString();
+                list.Add(ps);
+
+            }
+            con.Close();
+            return list;
+        }
+
+        public string InsertPassengers(Passenger pass)
+        {
+            SqlCommand cmd = new SqlCommand("sp_insert", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@name", pass.Name);
+            cmd.Parameters.AddWithValue("@address", pass.Address);
+            cmd.Parameters.AddWithValue("@phno", pass.Phone);
+            cmd.Parameters.AddWithValue("@email", pass.Email);
+            con.Open();
+
+            SqlParameter param = new SqlParameter()
+            {
+                ParameterName = "@msg",
+                Size = int.MaxValue,
+                Direction = System.Data.ParameterDirection.Output,
+            };
+            cmd.Parameters.Add(param);
+
+            cmd.ExecuteNonQuery();
+            return param.Value.ToString();
+
+        }
+
+        public string DeletePassenger(int id)
+        {
+            SqlCommand cmd = new SqlCommand("sp_deleted", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlParameter param = new SqlParameter()
+            {
+                ParameterName = "@msg",
+                Size = int.MaxValue,
+                Direction = System.Data.ParameterDirection.Output,
+            };
+            cmd.Parameters.Add(param);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            return param.Value.ToString();
+        }
+        public string UpdatePassenger(int id, Passenger pass)
+        {
+            SqlCommand cmd = new SqlCommand("sp_update", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@name", pass.Name);
+            cmd.Parameters.AddWithValue("@address", pass.Address);
+            cmd.Parameters.AddWithValue("@phone", pass.Phone);
+            cmd.Parameters.AddWithValue("@email", pass.Email);
+            SqlParameter param = new SqlParameter()
+            {
+                ParameterName = "@msg",
+                Size = int.MaxValue,
+                Direction = System.Data.ParameterDirection.Output,
+            };
+            cmd.Parameters.Add(param);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            return param.Value.ToString();
+        }
+
+        public Passenger StoreDetail(int id)
+        {
+            Passenger passenger = null;
+            SqlCommand cmd = new SqlCommand("select * from sales.Passenger where p_id=@id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                passenger = new Passenger();
+
+                passenger.Id = id;
+                passenger.Name = reader["p_name"].ToString();
+                passenger.Address = reader["address"].ToString();
+                passenger.Phone = reader["ph_no"].ToString();
+                passenger.Email = reader["email"].ToString();
+
+            }
+            con.Close();
+            return passenger;
+        }
+    }
+}
